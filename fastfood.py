9b51e472be07cae130b7bc205ff80727ea03c72d
Philip Otieno <tmitchellb007@gmail.com>
2018-09-11 09:40:59 +0300

    Added sessions

--- fastfood.py
+++ fastfood.py
@@ -31,13 +31,28 @@ def log_auth(username, password):
 			return True
 	return False
 
+#check if user is in session
+def check_user(func):
+	@wraps(func)
+	def wrap(*args, **kwargs):
+		if session["check_user"]:
+			return func(*args, **kwargs)
+		else:
+			return jsonify({'message' : "please login to continue"}), 401
+
+	return wrap
+	
 #Login if registered
 @app.route('/api/v1/login', methods=['POST'])
 def login():
 	username = request.get_json()['username']
 	password = request.get_json()['password']
-
-	return jsonify({'message' : 'welcome to Fast-Food-Fast'}), 200
+	if log_auth(username,password):
+		session['check_user'] = True
+		session['username'] = username
+		return jsonify({'message' : 'welcome to Fast-Food-Fast'}), 200
+	else:
+		return jsonify({'message' : "invalid details"}), 401
 
 #Initalization
 if __name__=="__main__":
